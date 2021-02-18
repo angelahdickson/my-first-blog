c0d04aaf0a667da97dd886deec75bf2428dac6ce
angelahdickson <angeladickson00@hotmail.co.uk>
2021-02-18 12:07:32 +0000

    Added view and template as well as CSS

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):
+class Post2(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
